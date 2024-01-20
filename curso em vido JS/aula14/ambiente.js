3bb3fd1993b53303a0fdc9daa960b162b2be0621
Anderson <andersonsds@outlook.com.br>
2024-01-20 14:10:45 -0300

    estudos

---
+++ curso em vido JS/aula14/ambiente.js
@@ -0,0 +1,12 @@
+var c = 1
+console.log(`usando while`)
+while (c <= 5){
+    console.log(c)
+    c++
+}
+
+//-------------
+console.log(`usando for`)
+for(var c = 1; c<=5;c++){
+    console.log(c)
+}
